6227c576e985dc092dd6c8e60215c8ff9e76da93
Ivan Tsyplenkov <vankas2010@yandex.ru>
2021-03-24 16:40:32 +0300

    First commit to my django proj

---
+++ DjangoWebProject1/app/views.py
@@ -0,0 +1,129 @@
+"""
+Definition of views.
+"""
+
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
+
+from django.contrib.auth.forms import UserCreationForm
+
+from .forms import AnketaForm
+
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Домашняя страница',
+            'year':datetime.now().year,
+        }
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Контакты',
+            'message':'Ваша страница контактов.',
+            'year':datetime.now().year,
+        }
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'О нас',
+            'message':'Страница с описанием приложения.',
+            'year':datetime.now().year,
+        }
+    )
+
+
+def links(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/links.html',
+        {
+            'title':'Полезные ресурсы',
+            'message':'Страница с полезными ресурсами.',
+            'year':datetime.now().year,
+        }
+    )
+
+
+def anketa(request):
+    assert isinstance(request, HttpRequest)
+    data = None
+    gender = {'1': 'Мужчина', '2': 'Женщина'}
+    internet = {'1': 'Каждый день', '2': 'Несколько раз в день', 
+                '3': 'Несколько раз в неделею', '4': 'Несколько раз в месяц'}
+    if request.method == 'POST':
+        form = AnketaForm(request.POST)
+        if form.is_valid():
+            data = dict()
+            data['name'] = form.cleaned_data['name']
+            data['city'] = form.cleaned_data['city']
+            data['job'] = form.cleaned_data['job']
+            data['gender'] = gender[ form.cleaned_data['gender'] ]
+            data['internet'] = internet[ form.cleaned_data['internet'] ]
+            if (form.cleaned_data['notice'] == True):
+                data['notice'] = 'Да'
+            else:
+                data[notice] = 'Нет'
+            data['email'] = form.cleaned_data['email']
+            data['message'] = form.cleaned_data['message']
+            form = None
+    else:
+        form = AnketaForm()
+    return render(
+        request,
+        'app/anketa.html',
+        {
+            'form': form,
+            'data': data
+        }
+    )
+
+
+def registration(request):
+    """Renders the registration page."""
+    
+    if request.method == "POST":    # после отправки формы
+        regform = UserCreationForm(request.POST)
+        if regform.is_valid():
+            reg_f = regform.save(commit=False)
+            reg_f.is_staff = False  # запрещен вход в административный раздел
+            reg_f.is_active = True  # active user
+            reg_f.is_superuser = False # not a superuser
+            reg_f.date_joined = datetime.now() # date of the registration
+            reg_f.last_login = datetime.now()   # date of the last authorization
+            
+            regform.save()          # save the changes after change some fields.
+
+            return redirect('home') # переадресация на главную страницу после регистрации.
+        else:
+            regform = UserCreationForm()    # create the form object for input data.
+
+        assert isinstance(request, HttpRequest)
+        return render(
+            request,
+            'app/registration.html',
+            {
+                'regform': regform,         # send the form to the pattern of web-page.
+                'year': datetime.now().year,
+            }
+        )
